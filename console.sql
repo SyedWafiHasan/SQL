22e69e738f349f53783a295b15a199aab649a65b
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-10-17 00:13:47 +0530

    Finished Section 2

--- console.sql
+++ console.sql
@@ -43,3 +43,35 @@ SELECT (customer_id), amount, payment_date FROM payment WHERE amount > 0.00 ORDE
 SELECT COUNT(*) FROM film WHERE length <= 50;
 
 --between
+SELECT * FROM payment WHERE amount BETWEEN 8 AND 12;
+SELECT COUNT(*) FROM payment WHERE amount BETWEEN 8 AND 12;
+SELECT * FROM payment WHERE payment_date BETWEEN '2007-02-01' AND '2007-02-15';
+SELECT * FROM payment WHERE payment_date BETWEEN '2007-02-01' AND '2007-02-16' ORDER BY payment_date;
+
+--IN
+SELECT * FROM payment WHERE amount IN (0.99, 1.98, 1.99) ORDER BY amount;
+
+--LIKE & ILIKE
+SELECT * FROM customer WHERE first_name LIKE 'J%';
+SELECT * FROM customer WHERE first_name LIKE 'J%' ORDER BY first_name;
+SELECT COUNT(*) FROM customer WHERE first_name LIKE 'J%';
+SELECT * FROM customer WHERE first_name LIKE 'J%' AND last_name LIKE 'S%' ORDER BY first_name;
+
+--GENERAL CHALLENGE
+--1. HOW MANY TRANSACTIONS GREATER THAN $5.00?
+SELECT COUNT(amount) FROM payment WHERE amount > 5;
+
+--2. HOW MANY ACTORS WITH FIRST NAME STARTING WITH P?
+SELECT COUNT(first_name) FROM actor WHERE first_name LIKE 'P%';
+
+--3. HOW MANY UNIQUE DISTRICTS ARE CUSTOMERS FROM?
+SELECT COUNT(DISTINCT(district)) FROM address;
+
+--4. NAME THE DISTINCT DISTRICTS
+SELECT DISTINCT(district) FROM address;
+
+--5. HOW MANY FILMS ARE R-RATED AND HAVE REPLACEMENT COST BETWEEN $5 AND $15
+SELECT COUNT(*) FROM film WHERE rating = 'R' AND replacement_cost BETWEEN 5 AND 15;
+
+--6. HOW MANY FILMS HAVE THE WORD 'TRUMAN' IN THE TITLE?
+SELECT title FROM film WHERE title ILIKE '%TRUMAN%';
