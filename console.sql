5050c376d0e8322a2c25edd56dbebcc6a4c8a8c1
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-10-16 12:59:05 +0530

    changes

--- console.sql
+++ console.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT first_name, last_name, email FROM customer;
 SELECT DISTINCT release_year FROM film;
 SELECT DISTINCT(release_year) FROM film;
 SELECT DISTINCT(rental_rate) FROM film;
-SELECT DISTINCT(rental_rate) FROM film ORDER BY rental_rate ASC;
+SELECT DISTINCT(rental_rate) FROM film ORDER BY rental_rate;
 
 SELECT DISTINCT(rating) FROM film;
 
@@ -27,3 +27,19 @@ SELECT film_id, title, rating, description FROM film WHERE rating = 'R' OR ratin
 SELECT email FROM customer WHERE first_name = 'Nancy' AND last_name = 'Thomas';
 SELECT title, description FROM film where title = 'Outlaw Hanky';
 SELECT name, phone, address FROM customer_list WHERE address = '259 Ipoh Drive';
+
+--order by
+SELECT * FROM customer ORDER BY store_id, first_name;
+SELECT store_id, first_name, last_name, customer_id FROM customer ORDER BY store_id;
+SELECT store_id, first_name, last_name, customer_id FROM customer ORDER BY store_id DESC, first_name;
+
+--limit
+SELECT * FROM payment ORDER BY payment_date DESC LIMIT 5;
+SELECT * FROM payment WHERE amount != 0.00 ORDER BY payment_date DESC LIMIT 5;
+SELECT * FROM payment LIMIT 0;
+
+--limit and order by challenge
+SELECT (customer_id), amount, payment_date FROM payment WHERE amount > 0.00 ORDER BY payment_date ASC LIMIT 10;
+SELECT COUNT(*) FROM film WHERE length <= 50;
+
+--between
