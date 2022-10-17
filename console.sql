9de7be0cef67f37942043faf78adfe721f4985cf
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-10-17 14:13:05 +0530

    Finished Section 4, completed Assessment and added .gitignore

--- console.sql
+++ console.sql
@@ -74,4 +74,92 @@ SELECT DISTINCT(district) FROM address;
 SELECT COUNT(*) FROM film WHERE rating = 'R' AND replacement_cost BETWEEN 5 AND 15;
 
 --6. HOW MANY FILMS HAVE THE WORD 'TRUMAN' IN THE TITLE?
-SELECT title FROM film WHERE title ILIKE '%TRUMAN%';
+SELECT title, description FROM film WHERE title ILIKE '%TRUMAN%';
+
+SELECT COUNT(*) FROM film WHERE description ILIKE '%DATABASE%';
+
+--AGGREGATE FUNCTIONS
+-- AGG FX CALLS ONLY HAPPEN IN SELECT OR HAVING CLAUSE
+SELECT MIN(replacement_cost) FROM film;
+SELECT MAX(replacement_cost) FROM film;
+SELECT MAX(replacement_cost), MIN(replacement_cost) FROM film;
+SELECT AVG(replacement_cost) FROM film;
+SELECT ROUND(AVG(replacement_cost), 3) FROM film;
+SELECT SUM(replacement_cost) FROM film;
+
+-- GROUP BY (ALSO AN AGG FX)
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount);
+
+SELECT customer_id, COUNT(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(amount);
+
+SELECT staff_id, customer_id, SUM(amount)
+FROM payment
+GROUP BY staff_id, customer_id
+ORDER BY customer_id, staff_id;
+
+SELECT DATE(payment_date), SUM(amount) FROM payment
+GROUP BY DATE(payment_date)
+ORDER BY SUM(amount);
+
+--GROUP BY CHALLENGES
+--1. WHICH EMPLOYEE GOT MOST NUMBER OF TRANSACTIONS
+SELECT staff_id, COUNT(amount) FROM payment
+GROUP BY staff_id
+ORDER BY COUNT(amount) DESC;
+
+--2. AVERAGE REPLACEMENT COST PER RATING
+SELECT rating, AVG(replacement_cost) FROM film
+GROUP BY rating;
+
+--3. TOP 5 CUSTOMERS WHO SPENT THE MOST
+SELECT customer_id, SUM(AMOUNT) FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC
+LIMIT 5;
+
+--HAVING
+SELECT customer_id, SUM(amount) FROM payment
+GROUP BY customer_id
+HAVING SUM(amount) > 100
+ORDER BY SUM(amount) DESC;
+
+SELECT store_id, COUNT(customer_id) FROM customer
+GROUP BY store_id
+HAVING COUNT(customer_id) > 300;
+
+--HAVING CHALLENGES
+--1. CUSTOMERS WITH 40 OR MORE TRANSACTIONS
+SELECT customer_id, COUNT(amount) FROM payment
+GROUP BY customer_id
+HAVING COUNT(amount) >= 40
+ORDER BY COUNT(amount) DESC;
+
+--2. CUSTOMERS WHO HAVE SPENT $100+ WITH STAFF MEMBER 2
+SELECT staff_id, customer_id, SUM(amount) FROM payment
+WHERE staff_id = 2
+GROUP BY staff_id, customer_id
+HAVING SUM(amount) > 100;
+
+--ASSESMENT TEST
+--1. CUSTOMERS WHO HAVE SPENT AT LEAST $110 WITH STAFF MEMBER 2
+SELECT staff_id, customer_id, SUM(amount) FROM payment
+WHERE staff_id = 2
+GROUP BY staff_id, customer_id
+HAVING SUM(amount) >= 110;
+
+--2. HOW MANY FILMS BEGIN WITH J?
+SELECT COUNT(title) FROM film
+WHERE title ILIKE 'J%';
+
+--3. CUSTOMER WITH HIGHEST CUSTOMER ID WHOSE NAME STARTS WITH E AND ADDRESS ID LOWER THAN 500
+SELECT customer_id, first_name, last_name FROM customer
+WHERE first_name ILIKE 'E%'
+AND address_id < 500 ORDER BY customer_id DESC
+LIMIT 1;
+
